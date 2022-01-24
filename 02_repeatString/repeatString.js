176f45059ca380c82ef420c4d4a0641215a2fce5
Eugen <jasonfilipss@yahoo.com>
2022-01-24 23:51:27 +0200

    second exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let startingString = string;
 
+
+    if (num < 0){
+        return 'ERROR';
+    }
+    else if (num == 0){
+        return '';
+    }
+
+    while (num > 1){
+        string += startingString;
+        num--;
+    }
+    return string;
 };
 
 // Do not edit below this line
