da566ee6650767b23760dab988d6963efa539888
Eugen <jasonfilipss@yahoo.com>
2022-01-25 16:35:33 +0200

    finished fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
 
+    if (a < 0 || b < 0 || typeof(a) != 'number' || typeof(b) != 'number'){
+        return 'ERROR';
+    }
+    else if (a < b){
+        while (a <= b){
+            sum += a ;
+            a++;
+        }
+    } else {
+        while (b <= a){
+            sum += b ;
+            b++;
+        }
+    }
+
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
