bc9b77285e5cce1b2e45afe9b0cade0e5e3ee443
Eugen <jasonfilipss@yahoo.com>
2022-01-25 16:56:56 +0200

    seventh exercide done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+ let goodUnits = (temp-32)*0.5556;
+ goodUnits = Math.round(goodUnits *10)/10;
+ return goodUnits;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+let badUnits = (temp*1.8)+32;
+badUnits = Math.round(badUnits*10)/10;
+return badUnits;
 };
 
 // Do not edit below this line
