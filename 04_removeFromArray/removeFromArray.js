8dc9cbdf6d225fe8ce4f53b04281ba55accaf05a
Eugen <jasonfilipss@yahoo.com>
2022-01-25 16:07:43 +0200

    finished fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    let array = args[0];
+    let arrayWithoutArgs = [];
+
+    array.forEach(element => {
+        if (!args.includes(element)){
+            arrayWithoutArgs.push(element);
+        }
+    });
+    
+    
+    return arrayWithoutArgs;
 };
+
+/*
+    for (let i = 0; i < array.length; i++){
+        for (let j = 0; j < argsArray.length; j++){
+            if (array[i] === argsArray[j]){
+                indexOfDeletedItem = array.indexOf(argsArray[j]);
+                array.splice(indexOfDeletedItem,1);
+            }
+        }
+    }
+
+
+    
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
